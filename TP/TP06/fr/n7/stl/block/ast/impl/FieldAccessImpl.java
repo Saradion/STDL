13298b67c271420a22f13ecd9198059912e1b647
Saradion <mthsbggnn@gmail.com>
2017-03-22 19:31:22 +0100

    Add TP06

---
+++ TP/TP06/fr/n7/stl/block/ast/impl/FieldAccessImpl.java
@@ -0,0 +1,56 @@
+package fr.n7.stl.block.ast.impl;
+
+import fr.n7.stl.block.ast.*;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+import javax.lang.model.type.ErrorType;
+import java.util.Optional;
+
+/**
+ * Implementation of the Abstract Syntax Tree node for accessing a field in a record.
+ * @author Marc Pantel
+ *
+ */
+public class FieldAccessImpl implements Expression {
+
+	private Expression record;
+	private String name;
+	private FieldDeclaration field;
+
+	public FieldAccessImpl(Expression _record, String _name) {
+		this.record = _record;
+		this.name = _name;
+	}
+
+	public FieldAccessImpl(Expression _record, FieldDeclaration _field) {
+		this.record = _record;
+		this.field = _field;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return this.record + "." + this.name;
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Expression#getType()
+	 */
+	@Override
+	public Type getType() {
+		Optional<FieldDeclaration> field_opt = ((RecordTypeImpl) record.getType()).get(name);
+		return field_opt.isPresent()? field_opt.get().getType():AtomicType.ErrorType;
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Expression#getCode(fr.n7.stl.tam.ast.TAMFactory)
+	 */
+	@Override
+	public Fragment getCode(TAMFactory _factory) {
+		throw new SemanticsUndefinedException( "getCode is undefined in FieldAccessImpl.");
+	}
+
+}
